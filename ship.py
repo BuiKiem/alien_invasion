8aae67faddf785f572725a4be90b511297cfc919
Bui Anh Huy <bahuy@tma.com.vn>
2019-04-20 12:38:36 +0700

    Adjust the Ship's Speed (p.253)

--- ship.py
+++ ship.py
@@ -3,8 +3,9 @@ import pygame
 
 class Ship:
 
-    def __init__(self, screen):
+    def __init__(self, ai_settings, screen):
         """Initialize the ship and set its starting position."""
+        self.ai_settings = ai_settings
         self.screen = screen
 
         # Load the ship image and get its rect.
@@ -16,6 +17,9 @@ class Ship:
         self.rect.centerx = self.screen_rect.centerx
         self.rect.bottom = self.screen_rect.bottom
 
+        # Store a decimal value for the ship's center
+        self.centerx = float(self.rect.centerx)
+
         # Movement flag
         self.moving_right = False
         self.moving_left = False
@@ -26,7 +30,12 @@ class Ship:
 
     def update(self):
         """Update the ship's position based on the movement flag."""
+
+        # Update the ship's center property, not the rect.
         if self.moving_right:
-            self.rect.centerx += 1
+            self.centerx += self.ai_settings.ship_speed_factor
         if self.moving_left:
-            self.rect.centerx -= 1
+            self.centerx -= self.ai_settings.ship_speed_factor
+
+        # Update rect object from self.center
+        self.rect.centerx = self.centerx
