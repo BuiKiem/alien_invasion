286e68f3088936be7d8fd7032db8d4945e902ce6
Bui Anh Huy <bahuy@tma.com.vn>
2019-04-20 11:23:31 +0700

    Create the Ship Class (p.245)

--- alien_invasion.py
+++ alien_invasion.py
@@ -1,6 +1,7 @@
 import sys
 
 import pygame
+
 from settings import Settings
 
 
@@ -11,7 +12,7 @@ def run_game():
     pygame.init()
     ai_settings = Settings()
     screen = pygame.display.set_mode(
-            (ai_settings.screen_width, ai_settings.screen_height))
+        (ai_settings.screen_width, ai_settings.screen_height))
     pygame.display.set_caption('Alien Invasion')
 
     # Start the main loop for the game
